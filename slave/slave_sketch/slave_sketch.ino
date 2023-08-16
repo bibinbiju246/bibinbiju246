0e19ddd5c8fc575796f0f78025ade434651cce66
BIBIN_B <bibinbiju246@gmail.com>
2023-08-16 17:12:31 +0530

    second chage

--- slave/slave_sketch/slave_sketch.ino
+++ slave/slave_sketch/slave_sketch.ino
@@ -3,14 +3,16 @@
 BluetoothSerial SerialBT;
 
 const int redledPin = 16; // LED pin
-const int buttonPin1 = 19;     // button for LED 1 control
-const int buttonPin2 = 4;     // button for LED 2 control
-const int buttonPin3 = 5;     // button for LED 3 control
-int brightness = 0;
+const int controlPin = 4;
+//const int buttonPin1 = 19;     // button for LED 1 control
+//const int buttonPin2 = 4;     // button for LED 2 control
+//const int buttonPin3 = 5;     // button for LED 3 control
+//int brightness = 0;
 
 void setup() {
   Serial.begin(115200);
   pinMode(redledPin, OUTPUT);
+  pinMode(controlPin,INPUT);
   
   SerialBT.begin("ESP32test", false); // true starts as slave
   Serial.println("The device started in slave mode.");
@@ -21,24 +23,34 @@ void setup() {
   Serial.println("Connected to master device!");
 }
 
-void loop() {
-  while (SerialBT.available()) {
-    char command = SerialBT.read();
-    if (digitalRead(buttonPin1) == LOW) {
-      digitalWrite(redledPin, HIGH); // Turn on LED
-    }
-    else if (digitalRead(buttonPin3) == LOW) {
-      digitalWrite(redledPin, LOW); // Turn off LED
-    }
-    else if (digitalRead(buttonPin2) == LOW) {
-      // Read the brightness level from master
-      while (!SerialBT.available()) {
-        delay(10);
-      }
-      brightness = SerialBT.parseInt();
-      brightness = constrain(brightness, 0, 255); // Limit brightness to 0-255
-      
-      analogWrite(redledPin, brightness); // Set LED brightness
-    }
+void loop() 
+
+{
+  if (digitalRead(controlPin) == HIGH) {
+    digitalWrite(redledPin, HIGH); // Turn on LED
+  } else {
+    digitalWrite(redledPin, LOW); // Turn off LED
   }
+  printf("chenges \n");
 }
+//{
+//  while (SerialBT.available()) {
+//    char command = SerialBT.read();
+//    if (digitalRead(buttonPin1) == LOW) {
+//      digitalWrite(redledPin, HIGH); // Turn on LED
+//    }
+//    else if (digitalRead(buttonPin3) == LOW) {
+//      digitalWrite(redledPin, LOW); // Turn off LED
+//    }
+//    else if (digitalRead(buttonPin2) == LOW) {
+//      // Read the brightness level from master
+//      while (!SerialBT.available()) {
+//        delay(10);
+//      }
+//      brightness = SerialBT.parseInt();
+//      brightness = constrain(brightness, 0, 255); // Limit brightness to 0-255
+//      
+//      analogWrite(redledPin, brightness); // Set LED brightness
+//    }
+//  }
+//}
